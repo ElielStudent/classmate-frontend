fe5b6b96d35ed537acf48f514e92c40ba4402cd9
ElielStudent <elielstudent@gmail.com>
2024-11-03 23:06:22 +0200

    Removed static exporting

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/classmate-frontend",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+
 };
 
 export default nextConfig;
